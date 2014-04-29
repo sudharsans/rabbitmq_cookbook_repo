20ff2abb4fb7fee0442d7606ee9113f32a006a7f
Sudharsan Sivasankaran <sudharsan@admins-MacBook-Pro-6.local>
2014-04-29 16:33:52 +0530

    wrapper updated

--- freshdesk_rabbitmq_wrapper/metadata.rb
+++ freshdesk_rabbitmq_wrapper/metadata.rb
@@ -1,9 +1,4 @@
 name             'freshdesk_rabbitmq_wrapper'
-maintainer       'YOUR_COMPANY_NAME'
-maintainer_email 'YOUR_EMAIL'
-license          'All rights reserved'
-description      'Installs/Configures freshdesk_rabbitmq_wrapper'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
 depends "rabbitmq"
