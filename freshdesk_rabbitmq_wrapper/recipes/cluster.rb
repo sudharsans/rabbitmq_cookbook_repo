24934d6e0cbf974383c5f0853ab62db69f3cc48b
Sudharsan Sivasankaran <sudharsan@admins-MacBook-Pro-6.local>
2014-05-02 09:07:36 +0530

    wrapper update

--- freshdesk_rabbitmq_wrapper/recipes/cluster.rb
+++ freshdesk_rabbitmq_wrapper/recipes/cluster.rb
@@ -1,7 +1,7 @@
 ruby_block "stop rabbitmq before change erlang_cookie" do
   block do	 
   end
-  notifies :stop, "service[rabbitmq-server]", :immediately    
+  notifies :stop, "service[rabbitmq-server]", :immediately 
 end
 
 directory "/var/lib/rabbitmq/mnesia" do
@@ -9,12 +9,18 @@ directory "/var/lib/rabbitmq/mnesia" do
   action :delete
 end
 
+file "/usr/lib/rabbitmq/.erlang.cookie" do
+   action :delete
+end
+
 template "/usr/lib/rabbitmq/.erlang.cookie" do
   source 'doterlang.cookie.erb'
   owner 'rabbitmq'
   group 'rabbitmq'
   mode 0040
-  notifies :restart, "service[rabbitmq-server]", :immediately   
+  action :create
+  notifies :start, "service[rabbitmq-server]", :immediately 
+  notifies :restart, "service[rabbitmq-server]", :immediately
 end
 
 template "/etc/rabbitmq/rabbitmq.config" do
@@ -22,14 +28,29 @@ template "/etc/rabbitmq/rabbitmq.config" do
   owner 'root'
   group 'root'
   mode 00644
-  notifies :run, "execute[reset-node]", :immediately
+ #notifies :stop, "service[rabbitmq-server]", :immediately
+  notifies :restart, "service[rabbitmq-server]", :immediately
+  sleep 130 
 end
-
-include_recipe 'freshdesk_rabbitmq_wrapper::user'
-include_recipe 'freshdesk_rabbitmq_wrapper::plugin'
 
 # Need to reset for clustering #
 execute 'reset-node' do
   command 'rabbitmqctl stop_app && rabbitmqctl reset && rabbitmqctl start_app'
-  action :nothing 
+  action :run 
+end
+
+include_recipe 'freshdesk_rabbitmq_wrapper::user'
+
+rabbitmq_plugin "rabbitmq_management" do
+  action :enable
+  notifies :restart, "service[rabbitmq-server]", :immediately 
+end
+
+service "rabbitmq-server" do
+      start_command 'setsid /etc/init.d/rabbitmq-server start'
+      stop_command 'setsid /etc/init.d/rabbitmq-server stop'
+      restart_command 'setsid /etc/init.d/rabbitmq-server restart'
+      status_command 'setsid /etc/init.d/rabbitmq-server status'
+      supports :status => true, :restart => true
+      action [:enable, :start]
 end
