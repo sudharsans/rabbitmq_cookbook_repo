16df3cdf030389efeedeb7ff520ce9b303915d7d
Sudharsan Sivasankaran <sudharsan@admins-MacBook-Pro-6.local>
2014-05-02 10:30:18 +0530

    wrapper update

--- freshdesk_rabbitmq_wrapper/recipes/install.rb
+++ freshdesk_rabbitmq_wrapper/recipes/install.rb
@@ -8,8 +8,20 @@
 #
 
 # Add all rabbitmq nodes to the hosts file with their short name.
+
+# see https://github.com/opscode-cookbooks/rabbitmq/blob/master/attributes/default.rb for more attributes.
+
+#get all instances from layers rabbit
+instances = node[:opsworks][:layers][:rabbit][:instances]
+rabbit_nodes = instances.map{ |name, attrs| "rabbit@#{name}" }
+
+node.set['rabbitmq']['cluster'] = true
+node.set['rabbitmq']['cluster_disk_nodes'] = rabbit_nodes
+node.set['rabbitmq']['erlang_cookie'] = 'AnyAlphaNumericStringWillDo'
+node.set['rabbitmq']['enabled_plugins'] = ['rabbitmq_managment']
+
 include_recipe 'rabbitmq::default'
 
 include_recipe 'freshdesk_rabbitmq_wrapper::user'
-include_recipe 'freshdesk_rabbitmq_wrapper::plugin'
+#include_recipe 'freshdesk_rabbitmq_wrapper::plugin'
 
