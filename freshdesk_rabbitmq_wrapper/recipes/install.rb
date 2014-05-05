c542676b705f828ae2319f8eb9ac2b6db2a6fcf7
Sudharsan Sivasankaran <sudharsan@admins-MacBook-Pro-6.local>
2014-05-05 15:49:14 +0530

    wrapper updated

--- freshdesk_rabbitmq_wrapper/recipes/install.rb
+++ freshdesk_rabbitmq_wrapper/recipes/install.rb
@@ -19,11 +19,16 @@ directory "/var/lib/rabbitmq/mnesia" do
   action :delete
 end
 
+file node['rabbitmq']['erlang_cookie_path'] do
+   action :delete
+end
+
 template node['rabbitmq']['erlang_cookie_path'] do
   source 'doterlang.cookie.erb'
   owner 'rabbitmq'
   group 'rabbitmq'
   mode 00400
+  action :create 
   notifies :start, "service[#{node['rabbitmq']['service_name']}]", :immediately
   notifies :restart, "service[rabbitmq-server]"   
 end
