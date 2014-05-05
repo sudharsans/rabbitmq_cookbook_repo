95e0d0254faf3827fc94c0c01d2c6032c2fcd661
Sudharsan Sivasankaran <sudharsan@admins-MacBook-Pro-6.local>
2014-05-05 12:40:28 +0530

    wrapper updated

--- freshdesk_rabbitmq_wrapper/recipes/cluster.rb
+++ freshdesk_rabbitmq_wrapper/recipes/cluster.rb
@@ -1,7 +1,7 @@
 ruby_block "stop rabbitmq before change erlang_cookie" do
   block do	 
   end
-  notifies :stop, "service[rabbitmq-server]", :immediately 
+  notifies :stop, "service[rabbitmq]", :immediately 
 end
 
 directory "/var/lib/rabbitmq/mnesia" do
@@ -19,6 +19,15 @@ template node['rabbitmq']['erlang_cookie_path'] do
   group 'rabbitmq'
   mode "0400"
   action :create
-  notifies :start, "service[rabbitmq-server]", :immediately 
-  notifies :restart, "service[rabbitmq-server]", :immediately
+  notifies :start, "service[rabbitmq]", :immediately 
+  notifies :restart, "service[rabbitmq]", :immediately
+end
+
+service "rabbitmq" do
+      start_command 'setsid /etc/init.d/rabbitmq-server start'
+      stop_command 'setsid /etc/init.d/rabbitmq-server stop'
+      restart_command 'setsid /etc/init.d/rabbitmq-server restart'
+      status_command 'setsid /etc/init.d/rabbitmq-server status'
+      supports :status => true, :restart => true
+      action [:enable, :start]
 end
